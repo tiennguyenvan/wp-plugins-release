1a8b7fd674b98e7a247e41d6a621daedaa69da99
tiennguyenvan <nguyentien.jobs@gmail.com>
2023-09-21 14:14:39 +0700

    New DragbLock

--- dragblock/dragblock/build/applications/editor-panel-attributes/server/attributes-render.php
+++ dragblock/dragblock/build/applications/editor-panel-attributes/server/attributes-render.php
@@ -10,18 +10,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 add_filter( 'render_block_data', 'dragblock_attributes_parsing', 10, 1 );
 /**
- * Check Documentation#143
+ * Check Documentation#153
  *
- * @param object|array|string $dragblock_0 check var-def#143.
+ * @param object|array|string $dragblock_0 check var-def#153.
  */
 function dragblock_attributes_parsing( $dragblock_0 ) {
 	if ( empty( $dragblock_0['attrs']['dragBlockAttrs'] ) ) {
 		return $dragblock_0;
 	}
-	// dev-reply#1413.
+	// dev-reply#1513.
 	$dragblock_1 = array();
 	foreach ( $dragblock_0['attrs']['dragBlockAttrs'] as $dragblock_2 ) {
-		// dev-reply#1417.
+		// dev-reply#1517.
 		if (
 			! empty( $dragblock_2['disabled'] ) ||
 			! isset( $dragblock_2['value'] ) ||
@@ -29,9 +29,9 @@ function dragblock_attributes_parsing( $dragblock_0 ) {
 		) {
 			continue;
 		}
-		// dev-reply#1426.
+		// dev-reply#1526.
 		if ( ! empty( $dragblock_2['locale'] ) ) {
-			// dev-reply#1431.
+			// dev-reply#1531.
 			if ( DRAGBLOCK_SITE_LOCALE === $dragblock_2['locale'] ) {
 				$dragblock_1[ $dragblock_2['slug'] ] = $dragblock_2['value'];
 			} elseif ( 'en_US' === $dragblock_2['locale'] && empty( $dragblock_1[ $dragblock_2['slug'] ] ) ) {
@@ -39,7 +39,7 @@ function dragblock_attributes_parsing( $dragblock_0 ) {
 			}
 			continue;
 		}
-		// dev-reply#1444.
+		// dev-reply#1544.
 		if ( 'sizes' === $dragblock_2['slug'] ) {
 			if ( ! empty( $dragblock_2['devices'] ) && strlen( $dragblock_2['devices'] ) < 3 ) {
 				if ( 'd' === $dragblock_2['devices'] ) {
@@ -62,27 +62,27 @@ function dragblock_attributes_parsing( $dragblock_0 ) {
 				}
 			}
 		}
-		// dev-reply#1469.
+		// dev-reply#1569.
 		if ( empty( $dragblock_1[ $dragblock_2['slug'] ] ) ) {
 			$dragblock_1[ $dragblock_2['slug'] ] = $dragblock_2['value'];
 		} else {
 			$dragblock_1[ $dragblock_2['slug'] ] .= ', ' . $dragblock_2['value'];
 		}
 	}
-	// dev-reply#1477.
+	// dev-reply#1577.
 	if ( 'dragblock/image' === $dragblock_0['blockName'] ) {
-		// dev-reply#1479.
+		// dev-reply#1579.
 		if ( empty( $dragblock_1['loading'] ) ) {
 			$dragblock_1['loading'] = 'lazy';
 		}
 		if ( empty( $dragblock_1['decoding'] ) ) {
 			$dragblock_1['decoding'] = 'async';
 		}
-		// dev-reply#1487.
+		// dev-reply#1587.
 		if ( ! empty( $dragblock_1['src'] ) ) {
-			// dev-reply#1489.
+			// dev-reply#1589.
 			if ( strpos( $dragblock_1['src'], '[dragblock.post.image.src' ) !== false ) {
-				// dev-reply#1491.
+				// dev-reply#1591.
 				if ( empty( $dragblock_1['alt'] ) ) {
 					$dragblock_1['alt'] = '[dragblock.post.title]';
 				}
@@ -96,49 +96,60 @@ function dragblock_attributes_parsing( $dragblock_0 ) {
 				}
 			}
 		}
+		if ( empty( $dragblock_1['alt'] ) ) {
+			$dragblock_4 = empty( $dragblock_1['src'] ) ? '' : esc_attr( basename( $dragblock_1['src'] ) );
+			$dragblock_1['alt'] = empty( $dragblock_4 ) ? esc_html__( 'Just a placeholder', 'dragblock' ) : $dragblock_4;
+		}
 	}
-	// dev-reply#14112.
-	$dragblock_4 = '';
+	// dev-reply#15117.
+	if ( 'dragblock/link' === $dragblock_0['blockName'] ) {
+		// dev-reply#15119.
+		if ( empty( $dragblock_1['aria-label'] ) ) {
+			$dragblock_1['aria-label'] = esc_html__( 'DragBlock Link Block', 'dragblock' );
+		}
+	}
+	// dev-reply#15125.
+	$dragblock_5 = '';
 	if ( ! empty( $dragblock_1['name'] ) && dragblock_is_reseved_terms( $dragblock_1['name'] ) ) {
 		$dragblock_1['name'] .= '__dragblock_wp_reseved_terms';
-		// dev-reply#14120.
+		// dev-reply#15133.
 	}
-	foreach ( $dragblock_1 as $dragblock_5 => $dragblock_6 ) {
-		$dragblock_4 .= ( sanitize_key( $dragblock_5 ) . '="' . esc_attr( do_shortcode( $dragblock_6 ) ) . '"' );
+	foreach ( $dragblock_1 as $dragblock_6 => $dragblock_7 ) {
+		$dragblock_5 .= ( sanitize_key( $dragblock_6 ) . '="' . esc_attr( do_shortcode( $dragblock_7 ) ) . '"' );
 	}
-	if ( $dragblock_4 ) {
-		$dragblock_0['attrs']['dragBlockParsedAttrs'] = $dragblock_4;
+	if ( $dragblock_5 ) {
+		$dragblock_0['attrs']['dragBlockParsedAttrs'] = $dragblock_5;
 	}
 	return $dragblock_0;
 }
 add_filter( 'render_block', 'dragblock_attributes_insert', 10, 2 );
 /**
- * Check Documentation#14104
+ * Check Documentation#15115
  *
- * @param object|array|string $dragblock_7 check var-def#14104.
- * @param object|array|string $dragblock_0 check var-def#14104.
+ * @param object|array|string $dragblock_8 check var-def#15115.
+ * @param object|array|string $dragblock_0 check var-def#15115.
  */
-function dragblock_attributes_insert( $dragblock_7, $dragblock_0 ) {
+function dragblock_attributes_insert( $dragblock_8, $dragblock_0 ) {
 	if ( empty( $dragblock_0['attrs']['dragBlockParsedAttrs'] ) ) {
-		return $dragblock_7;
+		return $dragblock_8;
 	}
-	$dragblock_8 = strpos( $dragblock_7, '<' );
-	if ( false === $dragblock_8 ) {
-		return $dragblock_7;
+	$dragblock_9 = strpos( $dragblock_8, '<' );
+	if ( false === $dragblock_9 ) {
+		return $dragblock_8;
 	}
-	$dragblock_9 = strpos( $dragblock_7, ' ', $dragblock_8 );
-	$dragblock_10 = strpos( $dragblock_7, '>', $dragblock_8 );
-	$dragblock_11 = $dragblock_9;
-	if ( false === $dragblock_11 ) {
-		$dragblock_11 = $dragblock_10;
-	} elseif ( false !== $dragblock_10 && $dragblock_10 < $dragblock_11 ) {
-		$dragblock_11 = $dragblock_10;
+	$dragblock_10 = strpos( $dragblock_8, ' ', $dragblock_9 );
+	$dragblock_11 = strpos( $dragblock_8, '>', $dragblock_9 );
+	$dragblock_12 = $dragblock_10;
+	if ( false === $dragblock_12 ) {
+		$dragblock_12 = $dragblock_11;
+	} elseif ( false !== $dragblock_11 && $dragblock_11 < $dragblock_12 ) {
+		$dragblock_12 = $dragblock_11;
 	}
-	if ( false === $dragblock_11 ) {
-		return $dragblock_7;
+	if ( false === $dragblock_12 ) {
+		return $dragblock_8;
 	}
-	return ( substr( $dragblock_7, 0, $dragblock_11 ) . ' ' .
+	return ( substr( $dragblock_8, 0, $dragblock_12 ) . ' ' .
 		$dragblock_0['attrs']['dragBlockParsedAttrs'] .
-		substr( $dragblock_7, $dragblock_11 )
+		substr( $dragblock_8, $dragblock_12 )
 	);
 }
--- dragblock/dragblock/readme.txt
+++ dragblock/dragblock/readme.txt
@@ -5,7 +5,7 @@ Tags: gutenberg, blocks, gutenberg blocks, editor, block, site builder, drag-and
 Requires at least: 5.9
 Requires PHP: 7.0
 Tested up to: 7.0
-Stable tag: 23.09.19
+Stable tag: 23.09.21
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
